53084c20083be8d9c210c9d3369625ec1ebcacfe
EdmundLimBoEn <lim_bo_en_edmund@s2025.ssts.edu.sg>
2025-08-25 16:50:58 +0800

    Initial Commit

---
+++ IOS INCheist Task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS INCheist Task
+//
+//  Created by Lim Bo En Edmund on 25/8/25.
+//
+import SwiftPersistence
+import SwiftUI
+
+struct ContentView: View {
+    @State var button_nonpersist = false
+    var body: some View {
+        VStack {
+            Image("kesler")
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS INCheist Task/IOS_INCheist_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_INCheist_TaskApp.swift
+//  IOS INCheist Task
+//
+//  Created by Lim Bo En Edmund on 25/8/25.
+//
+
+import SwiftUI
+
+@main
+struct IOS_INCheist_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
