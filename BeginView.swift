d4910a58610842967fc617819a32cc53a6dd839f
EdmundLimBoEn <lim_bo_en_edmund@s2025.ssts.edu.sg>
2025-09-01 16:10:45 +0800

    TEST

---
+++ BeginView.swift
@@ -0,0 +1,18 @@
+//
+//  BeginView.swift
+//  IOS INCheist Task
+//
+//  Created by Lim Bo En Edmund on 1/9/25.
+//
+
+import SwiftUI
+
+struct BeginView: View {
+    var body: some View {
+        Text("welcome to INCheist investigation")
+    }
+}
+
+#Preview {
+    BeginView()
+}
--- IOS INCheist Task/ContentView.swift
+++ IOS INCheist Task/ContentView.swift
@@ -3,19 +3,26 @@
 //  IOS INCheist Task
 //
 //  Created by Lim Bo En Edmund on 25/8/25.
-// test 2
+// test 4
 import SwiftPersistence
 import SwiftUI
 
 struct ContentView: View {
-    @State var button_nonpersist = false
     var body: some View {
-        VStack {
-            Image("kesler")
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack {
+                Image("kesler")
+                    .foregroundStyle(.tint)
+                Text("big kessler")
+                    .font(.largeTitle)
+                NavigationLink{
+                    BeginView()
+                } label: {
+                    Text("beninging")
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
